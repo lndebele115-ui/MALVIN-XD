a288b73a5ae8ad869288a0a3b354b6a669bdbb5b
lndebele115-ui <lndebele115@gmail.com>
2025-08-17 09:24:12 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ti8h3LAQ#2ddppzK7UmUOQ9M4hZUyWnJWxAEDXSlROcUan_6bAkM",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "27660899221", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
